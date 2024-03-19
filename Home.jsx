a6e995472b6a76db968d6beb6fef6b0d4c71a18e
Stanislav-Stiptsov <stanislav.stiptsov@gmail.com>
2024-03-19 11:14:49 +0200

    Second commit

--- Home.jsx
+++ Home.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
     return (
         <>
         <h1>Hello world!!!</h1>
-
+        <h1>Hello world!!!</h1>
         <h2>This app is only for pizza fans!</h2>
         </>
     )
